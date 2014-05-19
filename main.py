46c23529d34e41193ca38d0c9c5349ea70e2ea72
Imtiaz A. Nizami <imtiazan@gmail.com>
2014-05-19 12:51:27 +0500

    Initial working build.

---
+++ main.py
@@ -0,0 +1,128 @@
+'''
+Created on May 15, 2014
+
+@author: imtiaz.nizami
+@contact: imtiazan@gmail.com
+@version: 0.1
+@requires: Cell database file in the required format
+@requires: Neighbor file in the required format
+
+This script takes two 'csv' files as inputs and generates a TEMS readable cell file.
+The two 'csv' inputs file formats are:
+
+1. Cell Database File format:
+
+CELL,CI,LAT,LON,ANT_DIRECTION,ANT_BEAM_WIDTH,ARFCN,BSIC,MCC,MNC,LAC
+ABC1011,12341,24.30089209,67.53721882,0,40,64,26,410,6,235
+ABC1012,12342,24.30089209,67.53721882,120,40,75,65,410,6,235
+ABC1013,12343,24.30089209,67.53721882,240,40,71,11,410,6,235
+.
+.
+
+1. Neighbor File format:
+
+cell,nbr
+12341,23451
+12341,34562
+12341,45673
+.
+.
+
+'''
+import sys
+from PyQt4 import QtGui
+from window import Ui_MainWindow
+
+import os.path
+import csv
+import io
+
+class Main(QtGui.QMainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        self.ui.btnInputCellFile.clicked.connect(self.btnInputCellFile_Clicked)
+        self.ui.btnInputNeighborFile.clicked.connect(self.btnInputNeighborFile_Clicked)
+        self.ui.btnGenerateCellFile.clicked.connect(self.btnGenerateCellFile_Clicked)
+        self.ui.actionAbout.triggered.connect(self.actionAbout_Clicked)
+        
+    def btnInputCellFile_Clicked(self):
+        self.ui.txtInputCellFile.setText(QtGui.QFileDialog.getOpenFileName(filter='CSV (*.csv);;All files (*.*)'))
+              
+    def btnInputNeighborFile_Clicked(self):
+        self.ui.txtInputNeighborFile.setText(QtGui.QFileDialog.getOpenFileName(filter='CSV (*.csv);;All files (*.*)'))
+    
+    def actionAbout_Clicked(self):
+        QtGui.QMessageBox.information(None, 'About', "Source code for this software can be found at:\n Coming soon")
+        
+    def btnGenerateCellFile_Clicked(self):
+        fn_cel = self.ui.txtInputCellFile.text()
+        fn_nbr = self.ui.txtInputNeighborFile.text()
+        
+        # check if file / folder names are valid
+        if not os.path.isfile(fn_cel):
+            QtGui.QMessageBox.information(None, 'Warning', "Cell file name is invalid. Aborting execution.")
+            return
+            
+        if not os.path.isfile(fn_nbr):
+            QtGui.QMessageBox.information(None, 'Warning', "Neighbor file name is invalid. Aborting execution.")
+            return
+
+        # read neighbor file into a dictionary
+        # key: cell id
+        # value: list of neighbors 
+        dict_nbr = {}
+        
+        with open(fn_nbr, 'rt') as csvfile:
+            reader_nbr = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for row in reader_nbr:
+                if len(row) > 1:
+                    if row[0] not in dict_nbr:
+                        dict_nbr[row[0]] = []
+                    
+                    dict_nbr[ row[0].strip() ].append( row[1].strip() )   
+        
+        # read cell file and write to file like string
+        output = io.StringIO()
+        writer = csv.writer(output, quoting=csv.QUOTE_NONNUMERIC, lineterminator='\n')
+        writer.writerow(["2 TEMS_-_Cell_names"])
+        lst_header = []
+        flag = 1
+        
+        with open(fn_cel, 'rt') as f:
+            reader = csv.DictReader(f) # read rows into a dictionary format
+            for row in reader: # read a row as {column1: value1, column2: value2,...}                
+                lst = list(row.values())
+                for (k,v) in row.items(): # go over each column name and value  
+                    if flag:
+                        lst_header.append(k)                  
+                    if k == 'CI':
+                        if v in dict_nbr:
+                            lst = lst + dict_nbr[v][:32]
+                               
+                if flag:
+                    for n in range(1, 33):
+                        lst_header.append("CI_N_%s" % (n,))
+                    writer.writerow( lst_header )
+                    flag = 0
+                
+                writer.writerow( lst )
+        
+        # write output file
+        fn_out = QtGui.QFileDialog.getSaveFileName(filter='CEL (*.cel);;All files (*.*)')
+        if not os.path.isdir(os.path.dirname(fn_out)):
+            QtGui.QMessageBox.information(None, 'Warning', "Output file directory is invalid. Aborting execution.")
+            return
+        
+        file_out = open(fn_out, "w")
+        file_out.write(output.getvalue())
+        file_out.close()
+        
+        
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    window = Main()
+    window.show()
+    sys.exit(app.exec_())
---
+++ window.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'window.ui'
+#
+# Created: Mon May 19 11:09:29 2014
+#      by: PyQt4 UI code generator 4.9.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(499, 239)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(30, 50, 91, 20))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(50, 80, 61, 16))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.txtInputCellFile = QtGui.QLineEdit(self.centralwidget)
+        self.txtInputCellFile.setGeometry(QtCore.QRect(130, 50, 331, 20))
+        self.txtInputCellFile.setObjectName(_fromUtf8("txtInputCellFile"))
+        self.txtInputNeighborFile = QtGui.QLineEdit(self.centralwidget)
+        self.txtInputNeighborFile.setGeometry(QtCore.QRect(130, 80, 331, 20))
+        self.txtInputNeighborFile.setObjectName(_fromUtf8("txtInputNeighborFile"))
+        self.btnGenerateCellFile = QtGui.QPushButton(self.centralwidget)
+        self.btnGenerateCellFile.setGeometry(QtCore.QRect(320, 120, 161, 41))
+        self.btnGenerateCellFile.setObjectName(_fromUtf8("btnGenerateCellFile"))
+        self.btnInputCellFile = QtGui.QPushButton(self.centralwidget)
+        self.btnInputCellFile.setGeometry(QtCore.QRect(460, 50, 21, 21))
+        self.btnInputCellFile.setObjectName(_fromUtf8("btnInputCellFile"))
+        self.btnInputNeighborFile = QtGui.QPushButton(self.centralwidget)
+        self.btnInputNeighborFile.setGeometry(QtCore.QRect(460, 80, 21, 21))
+        self.btnInputNeighborFile.setObjectName(_fromUtf8("btnInputNeighborFile"))
+        self.label_3 = QtGui.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(180, 10, 191, 16))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setUnderline(True)
+        font.setWeight(50)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.label_5 = QtGui.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(330, 180, 151, 16))
+        font = QtGui.QFont()
+        font.setItalic(True)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName(_fromUtf8("label_5"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 499, 21))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.mnuAbout = QtGui.QMenu(self.menubar)
+        self.mnuAbout.setObjectName(_fromUtf8("mnuAbout"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionAbout = QtGui.QAction(MainWindow)
+        self.actionAbout.setObjectName(_fromUtf8("actionAbout"))
+        self.mnuAbout.addAction(self.actionAbout)
+        self.menubar.addAction(self.mnuAbout.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        MainWindow.setTabOrder(self.txtInputCellFile, self.btnInputCellFile)
+        MainWindow.setTabOrder(self.btnInputCellFile, self.txtInputNeighborFile)
+        MainWindow.setTabOrder(self.txtInputNeighborFile, self.btnInputNeighborFile)
+        MainWindow.setTabOrder(self.btnInputNeighborFile, self.btnGenerateCellFile)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.label.setText(_translate("MainWindow", "Cell Database File", None))
+        self.label_2.setText(_translate("MainWindow", "Neighbor File", None))
+        self.btnGenerateCellFile.setText(_translate("MainWindow", "Generate Cell File", None))
+        self.btnInputCellFile.setText(_translate("MainWindow", "...", None))
+        self.btnInputNeighborFile.setText(_translate("MainWindow", "...", None))
+        self.label_3.setText(_translate("MainWindow", "TEMS Cell File Generator", None))
+        self.label_5.setText(_translate("MainWindow", "Developed by Imtiaz A. Nizami", None))
+        self.mnuAbout.setTitle(_translate("MainWindow", "&File", None))
+        self.actionAbout.setText(_translate("MainWindow", "About", None))
+
